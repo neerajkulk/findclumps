4ac1ec4deb3cc67fe3b083fc29480f4c9440147a
Neeraj Kulkarni <neeraj.k@me.com>
2018-02-13 16:55:26 -0800

    fixed plot range.
    fixing the plot range does a better job at differentiating the adiabatic vs shattering sim

--- clump_finders/ytcode/mkpics.py
+++ clump_finders/ytcode/mkpics.py
@@ -3,17 +3,19 @@ import yt
 yt.enable_parallelism()
 import numpy as np
 
+# makes a 2048^2 image of simulation domain for making movies. There are no colorbars or axes.log(rho) is plotted on a black and white color scale.  
+
 def mkpic(fname):
-    # make a 2048^2 image of simulation domain for making movies (no colorbar or axes)                                                                                            
     ds = yt.load(fname)
     dim = ds.domain_dimensions[0]*(2**ds.max_level)
     slc = yt.SlicePlot(ds, 'z', 'density')
     slc.set_cmap('density', 'binary')
+    slc.set_zlim('density', 0.8, 100.0) # restrict plot range to 0.8 - 100.0. This plot range is good at showing shattering graphically
     slc.set_buff_size(2048)
     slc.hide_colorbar()
     slc.hide_axes()
     slc.save(fname + '.png')
-    # show a 2048 image of the center of simulation domain                                                                                return
+    return
 
 #mkpic("/Volumes/LaCie/simdata/production_runs/thin_8192/shatter.out3.00080.athdf")                                                        
 
